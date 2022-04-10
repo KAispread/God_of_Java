ece30ee7c9e7ff267562e33594760ae21fb73454
KAispread <pain0121@naver.com>
2022-04-11 01:19:26 +0900

    for test

---
+++ FirstJava/src/package01/Calculator.java
@@ -0,0 +1,11 @@
+package package01;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Calculator");
+		System.out.println("My name is 000");
+	}
+
+}
--- FirstJava/src/package01/HelloJava.java
+++ FirstJava/src/package01/HelloJava.java
@@ -9,4 +9,6 @@ public class HelloJava {
 		System.out.print("Me too");
 	}
 
+	
+	
 }
