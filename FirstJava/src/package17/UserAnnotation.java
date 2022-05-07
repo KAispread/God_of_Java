9a1f0431ffa7400a92d2cfc8ae0de98cc23679ec
KAispread <pain0121@naver.com>
2022-05-07 21:17:16 +0900

    Annotation

---
+++ FirstJava/src/package17/UserAnnotation.java
@@ -0,0 +1,13 @@
+package package17;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface UserAnnotation {
+	public int number();
+	public String text() default "This is first annotation";
+}
