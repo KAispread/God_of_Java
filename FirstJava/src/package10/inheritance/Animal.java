9bc44934a0750b4a43fc0d2fdbe0de78762fe5ec
KAispread <pain0121@naver.com>
2022-05-17 00:35:13 +0900

    study

--- FirstJava/src/package10/inheritance/Animal.java
+++ FirstJava/src/package10/inheritance/Animal.java
@@ -1,13 +1,15 @@
 package package10.inheritance;
 
 public class Animal {
-	
 	public String name;
 	public int legCount;
 	public int iq;
 	public boolean hasWing;
 	public int power;
 	
+	public Animal() {
+		
+	}
 	
 	public Animal(String name) {
 		this.name = name;
@@ -18,7 +20,7 @@ public class Animal {
 		this.legCount = legCount;
 		this.hasWing = hasWing;
 	}
-	
+
 	public Animal(String name, int legCount, boolean hasWing, int iq, int power) {
 		this.name = name;
 		this.legCount = legCount;
@@ -26,9 +28,7 @@ public class Animal {
 		this.iq = iq;
 		this.power = power;
 	}
-	
-	
-	
+
 	public void move() {
 		System.out.println(":has moved");
 	}
